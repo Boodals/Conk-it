aac034215fcfcd2df80af6d214c7770881a02f60
c3mundy <chantelle2.mundy@live.uwe.ac.uk>
2016-11-30 12:11:14 +0000

    Added a reset function to the HUD in case it's needed

--- Assets/Scripts/HUDScript.cs
+++ Assets/Scripts/HUDScript.cs
@@ -128,4 +128,14 @@ public class HUDScript : MonoBehaviour
                 break;
         }
     }
+
+    public void ResetHUD()
+    {
+        //incase we don't reload the scene
+        p1Score = 0;
+        p2Score = 0;
+        beginPos = background.transform.localPosition;
+        refreshHUD = true;
+        gameOver = false;
+    }
 }
